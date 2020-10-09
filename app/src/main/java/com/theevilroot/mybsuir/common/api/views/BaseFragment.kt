10652e4accd445d7620c25259c97d5edfa0c58bf
TheEvilRoot <robot-creatix@ya.ru>
2020-10-09 23:27:16 +0300

    Login in work

---
+++ app/src/main/java/com/theevilroot/mybsuir/common/api/views/BaseFragment.kt
@@ -0,0 +1,33 @@
+package com.theevilroot.mybsuir.common.api.views
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.fragment.app.Fragment
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.kodein
+
+open class BaseFragment (
+    @LayoutRes
+    val layoutRes: Int
+) : Fragment(), KodeinAware {
+
+    override val kodein: Kodein by kodein()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(layoutRes, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.onView()
+    }
+
+    open fun View.onView() { }
+
+}
---
+++ app/src/main/java/com/theevilroot/mybsuir/login/LoginFragment.kt
@@ -0,0 +1,11 @@
+package com.theevilroot.mybsuir.login
+
+import android.view.View
+import com.theevilroot.mybsuir.R
+import com.theevilroot.mybsuir.common.api.views.BaseFragment
+
+class LoginFragment : BaseFragment(R.layout.f_login) {
+    override fun View.onView() {
+
+    }
+}
